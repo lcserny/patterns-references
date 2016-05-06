bd8f20a9b9babd6a70411536db048518f8ffef5b
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-05-06 16:45:23 +0300

    small refactor prototype vone

--- prototype/src/main/java/net/cserny/prototype/vone/Command.java
+++ prototype/src/main/java/net/cserny/prototype/vone/Command.java
@@ -2,7 +2,6 @@ package net.cserny.prototype.vone;
 
 /**
  * Created by user on 06.05.2016.
- * 1. The clone() contract
  */
 public interface Command
 {
--- prototype/src/main/java/net/cserny/prototype/vone/Prototype.java
+++ prototype/src/main/java/net/cserny/prototype/vone/Prototype.java
@@ -2,6 +2,7 @@ package net.cserny.prototype.vone;
 
 /**
  * Created by user on 06.05.2016.
+ *  1. The clone() contract
  */
 public interface Prototype
 {
